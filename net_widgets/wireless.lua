b4746dfc57562c630b68b36ffd1805458a3e802f
plotnikovanton <plotnikovanton@gmail.com>
2015-01-20 15:38:51 +0300

    Network icons

--- net_widgets/wireless.lua
+++ net_widgets/wireless.lua
@@ -6,6 +6,10 @@ local beautiful    = require("beautiful")
 local widget = wibox.layout.fixed.horizontal()
 local connected = false
 
+-- Settings
+local ICON_DIR  = awful.util.getdir("config").."/net_widgets/icons/"
+local INTERFACE = "wlp1s0"
+
 local notification = nil
 function widget:hide() 
     if notification ~= nil then
@@ -19,7 +23,26 @@ function widget:show(t_out)
     
     local msg
     if connected then
-        msg = awful.util.pread(" iwconfig wlp1s0 | awk 'NR==1 {printf \"ESSID:\\t\\t%s\\nIIIE:\\t\\t%s\\n\", substr($4,8,length($4)-8), $3} NR==2 {printf \"Acces Point:\\t%s\", $6}' ")
+        local essid     = " N/A "
+        local mac       = " N/A "
+        local bitrate   = " N/A "   
+        local f = io.popen("iwconfig "..INTERFACE)
+       
+        line    = f:read()      -- wlp1s0    IEEE 802.11abgn  ESSID:"ESSID" 
+        essid   = string.match(line, "ESSID:\"(.+)\"") or essid
+        line    = f:read()      -- Mode:Managed  Frequency:2.437 GHz  Access Point: aa:bb:cc:dd:ee:ff
+        mac     = string.match(line, "Access Point: (.+)") or mac
+        line    = f:read()      -- Bit Rate=36 Mb/s   Tx-Power=15 dBm 
+        bitrate = string.match(line, "Bit Rate=(.+/s)") or bitrate
+
+        msg = 
+            "<span>".. -- configurable font here
+            "ESSID:\t\t"..essid.."\n"..
+            "BSSID:\t\t"..mac.."\n"..
+            "Bit rate:\t"..bitrate..
+            "</span>"
+
+        --msg = awful.util.pread(" iwconfig wlp1s0 | awk 'NR==1 {printf \"ESSID:\\t\\t%s\\nIIIE:\\t\\t%s\\n\", substr($4,8,length($4)-8), $3} NR==2 {printf \"Acces Point:\\t%s\", $6}' ")
     else
         msg = "Wireless network is disconnected"
     end
@@ -32,21 +55,32 @@ function widget:show(t_out)
 end
 
 local net_icon = wibox.widget.imagebox()
+net_icon:set_image(ICON_DIR.."wireless_na.png")
 local net_text = wibox.widget.textbox()
-local net_timer = timer({ timeout = 2 })
+net_text:set_text(" N/A ")
+local net_timer = timer({ timeout = 5 })
 local function net_update() 
-    local signal_level = awful.util.pread("awk 'NR==3 {printf \"%3.0f%\" ,($3/70)*100}' /proc/net/wireless")
-    if signal_level == '' then
+    local signal_level = tonumber(awful.util.pread("awk 'NR==3 {printf \"%3.0f\" ,($3/70)*100}' /proc/net/wireless"))
+    if signal_level == nil then
         connected = false
         net_text:set_text(" N/A ")
+        net_icon:set_image(ICON_DIR.."wireless_na.png")
     else
         connected = true
-        net_text:set_text(signal_level)
+        net_text:set_text(string.format("%3d%%", signal_level))
+        if signal_level < 25 then
+            net_icon:set_image(ICON_DIR.."wireless_0.png")
+        elseif signal_level < 50 then
+            net_icon:set_image(ICON_DIR.."wireless_1.png")
+        elseif signal_level < 75 then
+            net_icon:set_image(ICON_DIR.."wireless_2.png")
+        else 
+            net_icon:set_image(ICON_DIR.."wireless_3.png")
+        end
     end
 end
 
-net_icon:set_image(beautiful.widget_net)
-net_text:set_text(" N/A ")
+net_update()
 net_timer:connect_signal("timeout", net_update)
 net_timer:start()
 
