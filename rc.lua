d2d5cac0644900fa41dd8baf596f1bc55a9e14fe
Plotnikov Anton <plotnikovanton@gmail.com>
2015-03-27 16:49:12 +0300

    Overraide update_func

--- rc.lua
+++ rc.lua
@@ -4,6 +4,7 @@ local awful     = require("awful")
 awful.rules     = require("awful.rules")
 require("awful.autofocus")
 local wibox     = require("wibox")
+local common    = require("awful.widget.common")
 
 local beautiful = require("beautiful")
 beautiful.init(os.getenv("HOME") .. "/.config/awesome/theme/theme.lua")
@@ -503,7 +504,59 @@ for s = 1, screen.count() do
     mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, mytaglist.buttons)
 
     -- Create a tasklist widget
-    mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)--,wibox.layout.fixed.horizontal)
+    
+    local function tasklist_update(w, buttons, label, data, objects, nomargin)
+        -- update the widgets, creating them if needed
+        w:reset()
+        for i, o in ipairs(objects) do
+            local cache = data[o]
+            local ib, tb, bgb, m, l
+            if cache then
+                ib = cache.ib
+                tb = cache.tb
+                bgb = cache.bgb
+                m   = cache.m
+            else
+                ib = wibox.widget.imagebox()
+                tb = wibox.widget.textbox()
+                bgb = wibox.widget.background()
+                m = wibox.layout.margin(ib, 4, 4)
+                l = wibox.layout.fixed.horizontal()
+
+                -- All of this is added in a fixed widget
+                l:fill_space(true)
+                l:add(ib)
+                --l:add(m)
+
+                -- And all of this gets a background
+                bgb:set_widget(l)
+
+                bgb:buttons(common.create_buttons(buttons, o))
+
+                data[o] = {
+                    ib = ib,
+                    tb = tb,
+                    bgb = bgb,
+                    m   = m
+                }
+            end
+
+            local text, bg, bg_image, icon = label(o)
+            icon = icon or theme.tasklist_default_icon
+            -- The text might be invalid, so use pcall
+            if not pcall(tb.set_markup, tb, text) then
+                tb:set_markup("<i>&lt;Invalid text&gt;</i>")
+            end
+            bgb:set_bg(bg)
+            if type(bg_image) == "function" then
+                bg_image = bg_image(tb,o,m,objects,i)
+            end
+            bgb:set_bgimage(bg_image)
+            ib:set_image(icon)
+            w:add(bgb)
+        end
+    end
+    mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons, nil, tasklist_update, wibox.layout.fixed.horizontal())
 
     -- Create the wibox
     mywibox[s] = awful.wibox({ position = "top", screen = s, height = 18 })
